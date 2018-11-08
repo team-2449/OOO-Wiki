6e78af47fb032bc6f2bf87dd1c4468fa299d14a6
Alex Manning <you@example.com>
2018-11-08 02:51:09 -0700

    Added Basic Teleop

---
+++ robot-code-ooo/src/org/usfirst/frc/team2449/robot/commands/BasicTeleop.java
@@ -0,0 +1,47 @@
+package org.usfirst.frc.team2449.robot.commands;
+
+import org.usfirst.frc.team2449.robot.OI;
+import org.usfirst.frc.team2449.robot.Robot;
+import org.usfirst.frc.team2449.robot.subsystems.DriveTrain;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class BasicTeleop extends Command {
+
+	DriveTrain drivetrain;  //Robot Drivetrain
+	Joystick joystick;  //Driver Joystick
+	
+    public BasicTeleop() {
+    	drivetrain = Robot.drivetrain;
+    	joystick = OI.driverJoystick;
+        requires(drivetrain);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	//Set robot to do a basic tank drive
+    	drivetrain.driveDutyCycle(joystick.getRawAxis(1), joystick.getRawAxis(3));  //TODO:  Parameterize these axes in some way
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
