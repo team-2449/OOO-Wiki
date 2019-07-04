484ef374a9c4e263d718407a58b6f08d315f1131
alexmanning9904 <aemanni1@asu.edu>
2019-07-03 17:08:33 -0700

    Added functions assignment

---
+++ assignments/Functions.java
@@ -0,0 +1,21 @@
+class Functions{
+
+    public static void main(String[] args) {
+        //You will need to fill out the main method on your own.  Save this step for last.
+    }
+
+    double square(double a){
+        //make this function return the square of a
+    }
+
+    double log(double a, double base){
+        //make this function return the logarithm of a with some base
+    }
+
+    //add another math function here, from scratch.  Try to make it do multiple steps on the number.  Be creative.
+
+    double riemannSum(){
+        //take the riemann sum of your 3 functions in here.  You will need to add arguments to this function.  
+        //If you get stuck, review the last few lessons, and try to figure out how the concepts you already know can be used to solve this task.
+    }
+}
